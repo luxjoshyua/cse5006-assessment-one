804b753815b318cc3ca304be3770793584c425be
luxjoshyua <josh.e.fielding@gmail.com>
2026-08-07 07:23:11 +1000

    added video and updated readme

--- config/constants.ts
+++ config/constants.ts
@@ -13,4 +13,4 @@ export const SITE_METADATA = {
     "Frontend interface for an RSS Server feeding into a Learning Management System.",
 } as const
 
-export const VIDEO_ID = "6hqAdmKsyn4" as const
+export const VIDEO_ID = "NPWZSFZ5m-M" as const
